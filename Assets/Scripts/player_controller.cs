2c198343569ff7b51d9b21ddb7ad4c7256d2a8e8
TheLunaLongford <fagonzalezluna@gmail.com>
2023-06-22 00:51:53 -0600

    [ADD] plataformero: include player new input movement, and jump

--- Assets/Scripts/player_controller.cs
+++ Assets/Scripts/player_controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player_controller : MonoBehaviour
 {
@@ -13,18 +14,37 @@ public class player_controller : MonoBehaviour
     private Rigidbody2D playerRB;
     public LayerMask ground;
 
+    [SerializeField] float move_vector;
+    [SerializeField] float move_speed;
+    [SerializeField] float jump_speed;
 
+    Rigidbody2D rigib_body;
+    private SpriteRenderer sprite_renderer;
 
-    // Start is called before the first frame update
-    void Start()
+    void OnMove(InputValue value)
     {
-        
+        move_vector = value.Get<float>();
     }
+
+    void OnJump()
+    {
+        if (is_on_ground)
+        {
+            rigib_body.AddForce(Vector2.up * jump_speed, ForceMode2D.Impulse);
+        }
+    }
+
 
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        rigib_body = GetComponent<Rigidbody2D>();
+        sprite_renderer = GetComponent<SpriteRenderer>();
+        rigib_body.gravityScale = 4.7f;
+        move_speed = 10;
+        jump_speed = 20;
     }
 
     // Update is called once per frame
@@ -32,49 +52,57 @@ public class player_controller : MonoBehaviour
     {
         Debug.DrawRay(this.transform.position, Vector2.down * 1.0f, Color.red);
         is_character_on_floor();
-        if (Input.GetKeyDown(KeyCode.Space))
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    jump();
+        //}
+        //player_move();
+        transform.Translate(new Vector3(move_vector, 0, 0) * move_speed * Time.deltaTime);
+        if (move_vector > 0)
         {
-            jump();
+            sprite_renderer.flipX = false;
         }
-        player_move();
+        else if (move_vector < 0)
+        {
+            sprite_renderer.flipX = true;
+        }
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.2f, Color.red);
+        Debug.DrawRay(this.transform.position + (new Vector3(0.6f, 0, 0)), Vector2.down * 1.0f, Color.red);
+        Debug.DrawRay(this.transform.position + (new Vector3(-0.6f, 0, 0)), Vector2.down * 1.0f, Color.red);
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
 
     void is_character_on_floor()
     {
         if (
-            Physics2D.Raycast(this.transform.position, Vector2.down, 1.0f, ground)
-            || (Physics2D.Raycast(this.transform.position + (new Vector3(1f, 0, 0)), Vector2.down, 1.0f, ground))
-            || (Physics2D.Raycast(this.transform.position + (new Vector3(-1f, 0, 0)), Vector2.down, 1.0f, ground))
+            Physics2D.Raycast(this.transform.position, Vector2.down, 1.2f, ground)
+            || (Physics2D.Raycast(this.transform.position + (new Vector3(0.6f, 0, 0)), Vector2.down, 1.2f, ground))
+            || (Physics2D.Raycast(this.transform.position + (new Vector3(-0.6f, 0, 0)), Vector2.down, 1.2f, ground))
            )
         {
             is_on_ground = true;
-            animator.SetBool("on_air", false);
+            //animator.SetBool("on_air", false);
         }
         else
         {
             is_on_ground = false;
-            animator.SetBool("on_air", true);
+            //animator.SetBool("on_air", true);
         }
     }
 
-    void jump()
-    {
-        if (is_on_ground)
-        {
-            playerRB.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
-        }
-    }
+    //void jump()
+    //{
+    //    if (is_on_ground)
+    //    {
+    //        playerRB.AddForce(Vector2.up * jump_force, ForceMode2D.Impulse);
+    //    }
+    //}
 
-    void player_move()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+    //void player_move()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-        playerRB.AddForce(movement * player_speed);
-    }
+    //    Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+    //    playerRB.AddForce(movement * player_speed);
+    //}
 }
