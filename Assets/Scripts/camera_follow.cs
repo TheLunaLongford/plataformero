c3d14d1fb78fd08b7c87d918a5e657e8d54d5a41
TheLunaLongford <fagonzalezluna@gmail.com>
2023-06-23 22:52:00 -0600

    [ADD] plataformero: Include limits for lvl_1 camera to help limit level space

--- Assets/Scripts/camera_follow.cs
+++ Assets/Scripts/camera_follow.cs
@@ -9,6 +9,8 @@ public class camera_follow : MonoBehaviour
     public Vector3 offset = new Vector3(0.2f, 0.0f, -10.0f);
     public float damping_time = 0.3f;
     private Vector3 velocity = Vector3.zero;
+    public GameObject init_limit;
+    public GameObject finish_limit;
 
     private void Awake()
     {
@@ -17,7 +19,11 @@ public class camera_follow : MonoBehaviour
 
     private void move_camera(bool smooth)
     {
-        Vector3 destination = new Vector3(target.position.x - offset.x, offset.y, offset.z);
+        float target_x = target.position.x;
+        float init_x = init_limit.transform.position.x;
+        float finish_x = finish_limit.transform.position.x;
+        float destination_x = (target_x > init_x & target_x < finish_x ? target_x - offset.x : (target_x < init_x ? init_x : finish_x));
+        Vector3 destination = new Vector3(destination_x, offset.y, offset.z);
         if (smooth)
         {
             this.transform.position = Vector3.SmoothDamp(this.transform.position, destination, ref velocity, damping_time);
